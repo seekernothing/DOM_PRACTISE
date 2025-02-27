aa275f069ff4fcd0a8a61915f78e69d0d8102ace
ABhishek Biradar <abhishekbiradar2856@gmail.com>
2025-02-27 20:02:35 +0530

    Updated author details

---
+++ script7.js
@@ -0,0 +1,13 @@
+let pgb = document.querySelector("#progressbar");
+let h4 = document.querySelector("h4")
+
+let count =0
+let int=setInterval(function(){
+if(count===100){
+    clearInterval(int);
+    h4.style.opacity=1;
+
+}
+pgb.style.width = count + "%";
+count++
+},80)
